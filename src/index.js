ec33c7354d4374561de6967817d9ff1f396911d5
Jean Pierre <frankjeanpierregs@ufps.edu.co>
2020-06-10 12:17:02 -0500

    mejorando imagenes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-const express = require('express');
+
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
+
 
 //Setting
 app.set("port", process.env.PORT || 4000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 //Middlewares
 
 //Routes
@@ -16,5 +19,5 @@ app.use(express.static(path.join(__dirname, "public")));
 //Listening
 
 app.listen(app.get("port"), () => {
-    console.log("Server on port", app.get("port"))
+  console.log("Server on port", app.get("port"));
 });
